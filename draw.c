e5a34e409c7d594afe9f3ac6d0bb0802312a87f6
Dzyrus <snake.majestik@gmail.com>
2020-03-20 13:34:11 +0300

    draw

--- draw.c
+++ draw.c
@@ -34,19 +34,19 @@ void draw_line(float x, float y, float x1, float y1, t_fdf *fdf)
     }
 }
 
-void draw_map(t_fdf *fdf)
-{
-    int x;
-    int y;
-
-    y = 0;
-    while(fdf->z_values[y])
-    {
-        x = 0;
-        while(fdf->z_values[y][x] != '\0')
-        {
-            if(fdf->z_values[y])
-                draw_line(fdf->z_values[y][x],fdf);
-        }
-    }
-}
+//void draw_map(t_fdf *fdf)
+//{
+//    int x;
+//    int y;
+//
+//    y = 0;
+//    while(fdf->z_values[y])
+//    {
+//        x = 0;
+//        while(fdf->z_values[y][x] != '\0')
+//        {
+//            if(fdf->z_values[y])
+//                draw_line(fdf->z_values[y][x],fdf);
+//        }
+//    }
+//}
